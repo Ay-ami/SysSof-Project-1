d5fe94c601a8621e67156d13be5fdd4534d6ece5
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2020-06-04 23:30:27 -0400

    update main.c
    can create output1
    renamed the array of structs from the confusing "stack" to simply "IR", and properly builds the instruction array from the input file

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // given default values
 #define MAX_DATA_STACK_HEIGHT 1000;
 #define MAX_CODE_LENGTH 500;
 
 struct instruct{
-    int op; // opcode
-    int  l; // L lexigraphical level
-    int  m; // M
+    int OP; // opcode
+    int  L; // L lexigraphical level
+    int  M; // M
 }instruct;
 
 // opens the file
@@ -24,54 +25,117 @@ FILE *openFile(char fileName[], char mode[], FILE *fp)
     return fp;
 }
 
-// counts the number of lines in the input file
-int instructCount(FILE *fp)
+// returns the number of lines in the input file,
+int countLines(FILE *fp)
 {
     int count = 0;
     int temp;
+
     while(!feof(fp))
     {
-        // stores entire row made up of 3 integers into a temp variable to keep track of position
-      fscanf(fp, "%d%d%d", &temp, &temp, &temp);
+    // stores entire row made up of 3 integers into a temp variable to keep track of position
+      fscanf(fp, "%d %d %d", &temp, &temp, &temp);
       count++;
     }
+    // send file pointer back at the start so we can read it again
+    rewind(fp);
     return count;
 }
 
+// crates a string of interpreted assembly language with line number
+char *interpertOP(int op)
+{
+    // where a, b, and c are the read integers
+    // line, OP, L, M
+    char *OP = (char *)malloc(3);
+    switch (op)
+    {
+        case 1:
+            strcpy(OP, "LIT");
+            break;
+        case 2:
+            strcpy(OP, "OPR");
+            break;
+        case 3:
+            strcpy(OP, "LOD");
+            break;
+        case 4:
+            strcpy(OP, "STO");
+            break;
+        case 5:
+            strcpy(OP, "CAL");
+            break;
+        case 6:
+            strcpy(OP, "INC");
+            break;
+        case 7:
+            strcpy(OP, "JMP");
+            break;
+        case 8:
+            strcpy(OP, "JPC");
+            break;
+        case 9:
+            strcpy(OP, "SIO");
+            break;
+        case 10:
+            strcpy(OP, "SIO");
+            break;
+        case 11:
+            strcpy(OP, "SIO");
+            break;
+        default:
+            // some default/err case
+            break;
+    }
+    return OP;
+}
+
+void output1(struct instruct ir[], int size)
+{
+    FILE *fpw = openFile("output1.txt", "w", fpw);
+
+    fprintf(fpw, "Line, OP, L, M \n");
+
+    for (int i = 0; i<size; i++)
+    {
+        fprintf(fpw, "%3d ", i);
+        fprintf(fpw, "  %s ", interpertOP(ir[i].OP));
+        fprintf(fpw, "%d ", ir[i].L);
+        fprintf(fpw, "%d \n", ir[i].M);
+    }
+
+}
 int main()
 {
     // CPU Register
     int SP = MAX_DATA_STACK_HEIGHT;
     int BP = SP - 1;
     int PC = 0;
-    int IR = 0;
-
+    //int IR = 0;
+    int Hault = 1;
 
     FILE *fp;
-
-    // remember to keep "input.txt" in the same file as the main c file
-    // so that the compiler can find it
-    fp=openFile("input.txt", "r", fp);
-
-    int insCount = instructCount(fp);
-    printf("\nthe number of instructions is: %d \n", insCount);
-    rewind(fp);
+    fp=openFile("input.txt", "r", fp);// remember to keep "input.txt" in the same file as the main c file so that the compiler can find it
 
 
-    struct instruct stack[insCount];
+    // determining the total number of instructions
+    int num = countLines(fp); //num = number of instructions
 
-    // scan in and print out what has just been scanned
-    // again, this is only hard coded because I don't want to figure out the dynamic at this moment
-    fscanf(fp, "%d %d %d", &stack[0].op, &stack[0].l, &stack[0].m);
-    printf("%d %d %d\n", stack[0].op, stack[0].l, stack[0].m);
+    // Building and filling out the struct array
+    struct instruct IR[num];
+    int count = 0;
+    while(!feof(fp)) // "while not at the end of the file"
+    {
+      fscanf(fp, "%d %d %d", &IR[count].OP, &IR[count].L, &IR[count].M); // "scan in 3 numbers per line"
+      count++;
+    }
 
-    fscanf(fp, "%d %d %d", &stack[1].op, &stack[1].l, &stack[1].m);
-    printf("%d %d %d\n", stack[1].op, stack[1].l, stack[1].m);
+    // print output1 based on the now built IR array
+    output1(IR, num);
 
-    fscanf(fp, "%d %d %d", &stack[2].op, &stack[2].l, &stack[2].m);
-    printf("%d %d %d\n", stack[2].op, stack[2].l, stack[2].m);
 
-    switch(stack[0].op)
+    /* switch skeleton
+    switch(IR[0].OP)
     {
         case 1:
             // LIT	0, M	Push constant value (literal) M onto the stack
@@ -114,7 +178,7 @@ int main()
             // some default/err case
             break;
     }
-
+*/
 
     fclose(fp);
     return 0;
